d044f85ff68c93320d306e352293a0f440b7d95a
Rajdev Yadav <ryindia2023@gmail.com>
2023-04-14 12:28:36 -0400

    Todo Inputs & Button Styling Completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <Header />
+
+      <div className="container">
+        <div className="form">
+          <div className="fields">
+            <input type="text" placeholder='Enter Task' />
+            <input type="text" placeholder='Remarks' />
+          </div>
+          <div className="btns">
+            <button>Add</button>
+          </div>
+        </div>
+        <div className="result">
+
+        </div>
+      </div>
     </div>
   );
 }
